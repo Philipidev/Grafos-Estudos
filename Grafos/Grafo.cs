5c5ba4f7923c4aba9a814bfc9485f2708f919066
Philipidev <philipi.altoe@gmail.com>
2022-03-17 23:06:26 -0300

    first commit

---
+++ Grafos/Grafo.cs
@@ -0,0 +1,53 @@
+﻿namespace Grafos
+{
+    public class Grafo
+    {
+        private readonly int _tamano;
+        private readonly int[,] _grafo;
+
+        public Grafo(int Tamanho)
+        {
+            _tamano = Tamanho;
+            _grafo = IniciarGrafo(Tamanho);
+        }
+
+        private int[,] IniciarGrafo(int t)
+        {
+            int[,] arr = new int[t, t];
+
+            for (int i = 0; i < t; i++)
+            {
+                for (int j = 0; j < t; j++)
+                {
+                    arr[i, j] = 0;
+                }
+            }
+
+            return arr;
+        }
+
+        public void InserirAresta(int x, int y)
+        {
+            _grafo[x, y] += 1;
+            _grafo[y, x] += 1;
+        }
+
+        public int[] ExplorarProfundidade(int VerticeInicial)
+        {
+            List<int> Visitados = new List<int>();
+            _explorarProfundidade(Visitados, VerticeInicial);
+            return Visitados.ToArray();
+        }
+
+        private void _explorarProfundidade(List<int> Visitados, int atual)
+        {
+            Visitados.Add(atual);
+
+            for (int i = 0; i < _tamano; i++)
+            {
+                if (!Visitados.Any(a => a == i) && _grafo[atual, i] >= 1)
+                    _explorarProfundidade(Visitados, i);
+            }
+        }
+    }
+}
---
+++ Grafos/Program.cs
@@ -0,0 +1,15 @@
+﻿using Grafos;
+
+Grafo grafo = new Grafo(10);
+
+grafo.InserirAresta(1, 2);
+grafo.InserirAresta(2, 1);
+grafo.InserirAresta(2, 3);
+grafo.InserirAresta(3, 2);
+grafo.InserirAresta(5, 3);
+grafo.InserirAresta(3, 5);
+
+int[] visitados = grafo.ExplorarProfundidade(2);
+
+foreach (var v in visitados)
+    Console.WriteLine("Visitou {0}", v);
