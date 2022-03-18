ef6017ab8ff29653c00fc501a6838b702b717c1c
Philipidev <philipi.altoe@gmail.com>
2022-03-17 23:58:12 -0300

    melhorias

--- Grafos/GrafoMtz.cs
+++ Grafos/GrafoMtz.cs
@@ -1,35 +1,26 @@
 ﻿namespace Grafos
 {
-    public class Grafo
+    public class GrafoMtz
     {
         private readonly int _tamano;
         private readonly int[,] _grafo;
 
-        public Grafo(int Tamanho)
+        public GrafoMtz(int Tamanho)
         {
             _tamano = Tamanho;
-            _grafo = IniciarGrafo(Tamanho);
+            _grafo = _iniciarGrafo(Tamanho);
         }
 
-        private int[,] IniciarGrafo(int t)
-        {
-            int[,] arr = new int[t, t];
-
-            for (int i = 0; i < t; i++)
-            {
-                for (int j = 0; j < t; j++)
-                {
-                    arr[i, j] = 0;
-                }
-            }
-
-            return arr;
-        }
-
-        public void InserirAresta(int x, int y)
+        public void InserirArestaEspelhada(int x, int y)
         {
             _grafo[x, y] += 1;
             _grafo[y, x] += 1;
+        }
+
+        public void InserirAresta(int x1, int y1, int x2, int y2)
+        {
+            _grafo[x1, y1] += 1;
+            _grafo[x2, y2] += 1;
         }
 
         public int[] ExplorarProfundidade(int VerticeInicial)
@@ -48,6 +39,21 @@
                 if (!Visitados.Any(a => a == i) && _grafo[atual, i] >= 1)
                     _explorarProfundidade(Visitados, i);
             }
+        }
+
+        private int[,] _iniciarGrafo(int t)
+        {
+            int[,] arr = new int[t, t];
+
+            for (int i = 0; i < t; i++)
+            {
+                for (int j = 0; j < t; j++)
+                {
+                    arr[i, j] = 0;
+                }
+            }
+
+            return arr;
         }
     }
 }
--- Grafos/Program.cs
+++ Grafos/Program.cs
@@ -1,13 +1,13 @@
 ﻿using Grafos;
 
-Grafo grafo = new Grafo(10);
+GrafoMtz grafo = new GrafoMtz(10);
 
-grafo.InserirAresta(1, 2);
-grafo.InserirAresta(2, 1);
-grafo.InserirAresta(2, 3);
-grafo.InserirAresta(3, 2);
-grafo.InserirAresta(5, 3);
-grafo.InserirAresta(3, 5);
+grafo.InserirArestaEspelhada(1, 2);
+grafo.InserirArestaEspelhada(2, 1);
+grafo.InserirArestaEspelhada(2, 3);
+grafo.InserirArestaEspelhada(3, 2);
+grafo.InserirArestaEspelhada(5, 3);
+grafo.InserirArestaEspelhada(3, 5);
 
 int[] visitados = grafo.ExplorarProfundidade(2);
 
